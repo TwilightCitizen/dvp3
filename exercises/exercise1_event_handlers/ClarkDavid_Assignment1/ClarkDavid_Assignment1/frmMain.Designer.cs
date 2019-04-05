f2066f6ea1249fdabcd7c36f6cd7c6c34bdac2dd
David Clark <daclark1@student.fullsail.edu>
2019-04-05 18:14:59 -0400

    Initial/Launch form interface, client resizing, and borderless movement complete.

---
+++ exercises/exercise1_event_handlers/ClarkDavid_Assignment1/ClarkDavid_Assignment1/frmMain.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace ClarkDavid_Assignment1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.grpMain = new System.Windows.Forms.GroupBox();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPrint = new System.Windows.Forms.ToolStripMenuItem();
+            this.sep1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lstPending = new System.Windows.Forms.ListBox();
+            this.lstComplete = new System.Windows.Forms.ListBox();
+            this.lblPending = new System.Windows.Forms.Label();
+            this.lblComplete = new System.Windows.Forms.Label();
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.grpMain.SuspendLayout();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpMain
+            // 
+            this.grpMain.BackColor = System.Drawing.Color.Transparent;
+            this.grpMain.Controls.Add(this.button2);
+            this.grpMain.Controls.Add(this.button1);
+            this.grpMain.Controls.Add(this.btnDelete);
+            this.grpMain.Controls.Add(this.btnSwap);
+            this.grpMain.Controls.Add(this.lblComplete);
+            this.grpMain.Controls.Add(this.lblPending);
+            this.grpMain.Controls.Add(this.lstComplete);
+            this.grpMain.Controls.Add(this.lstPending);
+            this.grpMain.Controls.Add(this.mnuMain);
+            this.grpMain.Location = new System.Drawing.Point(47, 172);
+            this.grpMain.Name = "grpMain";
+            this.grpMain.Size = new System.Drawing.Size(574, 964);
+            this.grpMain.TabIndex = 0;
+            this.grpMain.TabStop = false;
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.ImageScalingSize = new System.Drawing.Size(36, 36);
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.mnuMain.Location = new System.Drawing.Point(3, 31);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(568, 47);
+            this.mnuMain.TabIndex = 0;
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuLoad,
+            this.mnuSave,
+            this.mnuPrint,
+            this.sep1,
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(70, 43);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuLoad
+            // 
+            this.mnuLoad.Name = "mnuLoad";
+            this.mnuLoad.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.mnuLoad.Size = new System.Drawing.Size(360, 42);
+            this.mnuLoad.Text = "&Load";
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mnuSave.Size = new System.Drawing.Size(360, 42);
+            this.mnuSave.Text = "&Save";
+            // 
+            // mnuPrint
+            // 
+            this.mnuPrint.Name = "mnuPrint";
+            this.mnuPrint.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.mnuPrint.Size = new System.Drawing.Size(360, 42);
+            this.mnuPrint.Text = "&Print";
+            // 
+            // sep1
+            // 
+            this.sep1.Name = "sep1";
+            this.sep1.Size = new System.Drawing.Size(357, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.mnuExit.Size = new System.Drawing.Size(360, 42);
+            this.mnuExit.Text = "E&xit";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // lstPending
+            // 
+            this.lstPending.FormattingEnabled = true;
+            this.lstPending.ItemHeight = 29;
+            this.lstPending.Location = new System.Drawing.Point(6, 137);
+            this.lstPending.Name = "lstPending";
+            this.lstPending.Size = new System.Drawing.Size(280, 758);
+            this.lstPending.TabIndex = 1;
+            // 
+            // lstComplete
+            // 
+            this.lstComplete.FormattingEnabled = true;
+            this.lstComplete.ItemHeight = 29;
+            this.lstComplete.Location = new System.Drawing.Point(288, 137);
+            this.lstComplete.Name = "lstComplete";
+            this.lstComplete.Size = new System.Drawing.Size(280, 758);
+            this.lstComplete.TabIndex = 3;
+            // 
+            // lblPending
+            // 
+            this.lblPending.AutoSize = true;
+            this.lblPending.Location = new System.Drawing.Point(6, 105);
+            this.lblPending.Name = "lblPending";
+            this.lblPending.Size = new System.Drawing.Size(103, 29);
+            this.lblPending.TabIndex = 0;
+            this.lblPending.Text = "&Pending";
+            // 
+            // lblComplete
+            // 
+            this.lblComplete.AutoSize = true;
+            this.lblComplete.Location = new System.Drawing.Point(283, 105);
+            this.lblComplete.Name = "lblComplete";
+            this.lblComplete.Size = new System.Drawing.Size(118, 29);
+            this.lblComplete.TabIndex = 2;
+            this.lblComplete.Text = "&Complete";
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.Enabled = false;
+            this.btnSwap.Location = new System.Drawing.Point(148, 901);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(135, 57);
+            this.btnSwap.TabIndex = 5;
+            this.btnSwap.Text = "&Swap";
+            this.btnSwap.UseVisualStyleBackColor = true;
+            this.btnSwap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(432, 901);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(135, 57);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "&Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(290, 901);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 57);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "&Edit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 901);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 57);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "&Add";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(659, 1272);
+            this.Controls.Add(this.grpMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exercise 1: Event Handlers";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseUp);
+            this.grpMain.ResumeLayout(false);
+            this.grpMain.PerformLayout();
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpMain;
+        private System.Windows.Forms.Label lblComplete;
+        private System.Windows.Forms.Label lblPending;
+        private System.Windows.Forms.ListBox lstComplete;
+        private System.Windows.Forms.ListBox lstPending;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuLoad;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuPrint;
+        private System.Windows.Forms.ToolStripSeparator sep1;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.Button btnSwap;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ exercises/exercise1_event_handlers/ClarkDavid_Assignment1/ClarkDavid_Assignment1/frmMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClarkDavid_Assignment1
+{
+    public partial class frmMain : Form
+    {
+        private bool  CaptureMove { get; set; } = false;
+        private Point Origin      { get; set; }
+
+        public frmMain()
+        {
+            InitializeComponent();
+            FixClientWinSize();
+        }
+
+        private void frmMain_Load( object sender, EventArgs e )
+        {
+
+        }
+
+        // The following 4 methods probably warrant subclassing.
+
+        // More concise client size fixer. 
+        private void FixClientWinSize()
+        {
+            Size  size   = Screen.PrimaryScreen.WorkingArea.Size;
+            int   height = Convert.ToInt32( size.Height / 1.4f );
+            int   width  = Convert.ToInt32( size.Width  / 6.0f  );
+
+                  height = height < Size.Height ? Size.Height : height;
+                  width  = width  < Size.Width  ? Size.Width  : width;
+                  Size   = new Size( width, height );
+        }
+
+        // Prepare to capture form movement.
+        private void frmMain_MouseDown( object sender, MouseEventArgs e )
+        {
+            CaptureMove = true;
+            Origin      = e.Location;
+        }
+
+        // Capture form movement.
+        private void frmMain_MouseMove( object sender, MouseEventArgs e )
+        {
+            if( CaptureMove ) Location =  new Point( Location.X - Origin.X + e.X
+                                                   , Location.Y - Origin.Y + e.Y );
+            Update();
+        }
+
+        // Stop capturing form movement.
+        private void frmMain_MouseUp( object sender, MouseEventArgs e )
+        {
+            CaptureMove = false;
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+
+        }
+
+        private void mnuExit_Click( object sender, EventArgs e )
+        {
+            Close();
+        }
+    }
+}
