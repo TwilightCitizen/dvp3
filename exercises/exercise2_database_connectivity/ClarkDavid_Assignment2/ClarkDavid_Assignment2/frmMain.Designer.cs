2b3366ce17f41f46ca4fb11074efbf7ce8001e51
David Clark <daclark1@student.fullsail.edu>
2019-04-08 14:06:16 -0400

    Got client size scaling working for my non-Retina display.

---
+++ exercises/exercise2_database_connectivity/ClarkDavid_Assignment2/ClarkDavid_Assignment2/frmMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿/* Name:     David A. Clark, Jr.
+ * Student:  0004796375
+ * Class:    Development Portfolio 3 (MDV239-O)
+ * Term:     C201904-01
+ * Exercise: Database Connectivity
+ * Date:     April 8, 2019 */
+
+namespace ClarkDavid_Assignment2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.grpContent = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.grpContent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpContent
+            // 
+            this.grpContent.BackColor = System.Drawing.Color.Transparent;
+            this.grpContent.Controls.Add(this.button3);
+            this.grpContent.Controls.Add(this.textBox2);
+            this.grpContent.Controls.Add(this.textBox1);
+            this.grpContent.Controls.Add(this.button2);
+            this.grpContent.Controls.Add(this.button1);
+            this.grpContent.Location = new System.Drawing.Point(50, 186);
+            this.grpContent.Name = "grpContent";
+            this.grpContent.Size = new System.Drawing.Size(595, 1019);
+            this.grpContent.TabIndex = 0;
+            this.grpContent.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(365, 585);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 110);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(26, 944);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(334, 47);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Testing 123";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(197, 424);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 47);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Testing 123";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(159, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 110);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(17, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(687, 1351);
+            this.Controls.Add(this.grpContent);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "D";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseUp);
+            this.grpContent.ResumeLayout(false);
+            this.grpContent.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpContent;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ exercises/exercise2_database_connectivity/ClarkDavid_Assignment2/ClarkDavid_Assignment2/frmMain.cs
@@ -0,0 +1,101 @@
+﻿/* Name:     David A. Clark, Jr.
+ * Student:  0004796375
+ * Class:    Development Portfolio 3 (MDV239-O)
+ * Term:     C201904-01
+ * Exercise: Database Connectivity
+ * Date:     April 8, 2019 */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClarkDavid_Assignment2
+{
+    public partial class frmMain : Form
+    {
+        /* Form Movement Capture */
+
+        private bool  CaptureMove { get; set; } = false;
+        private Point Origin      { get; set; }
+
+        /* Constructors */
+
+        public frmMain()
+        {
+            InitializeComponent();
+            ScaleToFitScreen( this );
+        }
+
+        /* Handlers to Support Movement of Borderless Form */
+
+        /* Prepare to capture form movement. */
+
+        private void frmMain_MouseDown( object sender, MouseEventArgs e )
+        {
+            CaptureMove = true;
+            Origin      = e.Location;
+        }
+
+        /* Capture form movement. */
+
+        private void frmMain_MouseMove( object sender, MouseEventArgs e )
+        {
+            if( CaptureMove ) Location =  new Point( Location.X - Origin.X + e.X
+                                                   , Location.Y - Origin.Y + e.Y );
+            Update();
+        }
+
+        /* Stop capturing form movement. */
+
+        private void frmMain_MouseUp( object sender, MouseEventArgs e )
+        {
+            CaptureMove = false;
+        }
+
+        /* Scale the specified control and its constituents by the specified factor,
+         * iterating through all decendent controls, scaling their font by the
+         * specified factor, too.  The default factor is 75% if none is specified.
+         * 
+         * This works better than Keith Websters method for fixing the client window
+         * size when dealing with displays at different resolutions and DPIs.  One
+         * caveat is that display scaling on the Windows VM needs to be set to 100%,
+         * or Visual Studio starts borking the proportions in the designer.
+         * 
+         * I am now running a UWQHD display at 3440x1440 resolution, so the
+         * specified iPhone image and window sizes exceed the height of my screen,
+         * making things look very clunky.  Calling this method after initialization
+         * scales it down so it fits.  On the MacBook Pro's Retina Display at 2880x
+         * 1800 resolution, the screen exceeds the bounds of the iPhone image and
+         * window, so no scaling need occur. */
+
+        private void ScaleToFitScreen( Control control, float factor = 0.75f )
+        {
+            if( Height > Screen.PrimaryScreen.WorkingArea.Size.Height )
+            {
+                Scale( new SizeF( factor, factor ) );
+                
+                Action< Control > ScaleChildren = null;
+                
+                ScaleChildren = ( Control parent ) =>
+                {
+                    foreach( Control child in parent.Controls)
+                    {
+                        Font font = child.Font;
+
+                        child.Font = new Font( font.Name, font.SizeInPoints * factor * factor );
+
+                        ScaleChildren( child );
+                    }
+                };
+
+                ScaleChildren( control );
+            }
+        }
+    }
+}
