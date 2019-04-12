7cf35a884a1e8d2ca90eb4aeb1c54fb7b44113a4
David Clark <daclark1@student.fullsail.edu>
2019-04-12 18:20:37 -0400

    Fleshing out Keep'n Tabs' web API server.  So far, it's merely a driver to ensure the Simple API class library is working correctly.  With a solid driver implementation working, I see where more functionality can be potentially pushed into Simple API.

---
+++ final_project/keepn_tabs_api/KeepnTabsAPI/KeepnTabsAPI/frmController.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace KeepnTabsAPI
+{
+    partial class frmController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(111, 52);
+            this.btnStartStop.Margin = new System.Windows.Forms.Padding(1);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(72, 29);
+            this.btnStartStop.TabIndex = 1;
+            this.btnStartStop.Text = "&Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // frmController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 132);
+            this.Controls.Add(this.btnStartStop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmController";
+            this.Text = "Keep\'n Tabs API Server Controller";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+    }
+}
+
---
+++ final_project/keepn_tabs_api/KeepnTabsAPI/KeepnTabsAPI/frmController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleAPI;
+
+namespace KeepnTabsAPI
+{
+    public partial class frmController : Form
+    {
+        private Server Server = new Server( "http://192.168.82.128:8080/todo/" );
+
+        public frmController()
+        {
+            InitializeComponent();
+
+            Server.RequestArrived += Server_RequestArrived;
+        }
+
+        private void Server_RequestArrived( object sender, Exchange e )
+        {
+            var requests = new Dictionary< string, Dictionary< string, Action< Exchange > > >()
+            {
+                [ "user" ] = new Dictionary< string, Action< Exchange > >()
+                {
+                    [ "add" ]     = UserAdd,
+                    [ "confirm" ] = UserConfirm,
+                    [ "update" ]  = UserUpdate,
+                    [ "delete" ]  = UserDelete
+                },
+                [ "list" ] = new Dictionary< string, Action< Exchange > >()
+                {
+                    [ "add" ]     = ListAdd,
+                    [ "update" ]  = ListUpdate,
+                    [ "delete" ]  = ListDelete
+                },
+                [ "task" ] = new Dictionary< string, Action< Exchange > >()
+                {
+                    [ "add" ]     = TaskAdd,
+                    [ "update" ]  = TaskUpdate,
+                    [ "delete" ]  = TaskDelete
+                }
+            };
+
+            try
+            {
+                requests[ e.Request[ 0 ] ][ e.Request[ 1 ] ]( e );
+            }
+            catch
+            {
+                Invalid( e );
+            }
+        }
+
+        private void UserAdd( Exchange e )
+        {
+            e.Reply = "User Add";
+        }
+
+        private void UserConfirm( Exchange e )
+        {
+            e.Reply = "User Confirm";
+        }
+
+        private void UserUpdate( Exchange e )
+        {
+            e.Reply = "User Update";
+        }
+
+        private void UserDelete( Exchange e )
+        {
+            e.Reply = "User Delete";
+        }
+
+        private void ListAdd( Exchange e )
+        {
+            e.Reply = "List Add";
+        }
+
+        private void ListUpdate( Exchange e )
+        {
+            e.Reply = "List Update";
+        }
+
+        private void ListDelete( Exchange e )
+        {
+            e.Reply = "List Delete";
+        }
+
+        private void TaskAdd( Exchange e )
+        {
+            e.Reply = "Task Add";
+        }
+
+        private void TaskUpdate( Exchange e )
+        {
+            e.Reply = "Task Update";
+        }
+
+        private void TaskDelete( Exchange e )
+        {
+            e.Reply = "Task Delete";
+        }
+
+        private void Invalid( Exchange e )
+        {
+            e.Reply = "Invalid Request";
+        }
+
+        private void btnStartStop_Click( object sender, EventArgs e )
+        {
+            btnStartStop.Text = Server.IsRunning ? "&Start" : "&Stop";
+
+            if( Server.IsRunning ) Server.Stop(); else Server.Start();
+        }
+    }
+}
