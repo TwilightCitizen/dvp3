bdb1813282490fc8cc154af3a62b82c2ca86e03a
David Clark <daclark1@student.fullsail.edu>
2019-04-15 23:25:38 -0400

    Rudimentary functionality working after setting up event handlers, common methods for related ones, and adding all the buttons to collections for smart testing.
    Clicking a button sets it's tag to the selected piece (all x's at the moment).  When a winning run occurs, it reports the win.

--- exercises/exercise3_fill_in_the_logic/TicTacToe/TicTacToe/Form1.cs
+++ exercises/exercise3_fill_in_the_logic/TicTacToe/TicTacToe/Form1.cs
@@ -12,28 +12,170 @@ namespace TicTacToe
 {
     public partial class frmTicTacToe : Form
     {
-        // NAME:
-        // CLASS AND TERM:
-        // PROJECT: Tic Tac Toe
+        private enum Pieces            { x, o, z }
 
-        /* THINGS TO CONSIDER:
-            - You must change the project name to conform to the required
-              naming convention.
-            - You must comment the code throughout.  Failure to do so could result
-              in a lower grade.
-            - All button names and other provided variables and controls must
-              remain the same.  Changing these could result in a 0 on the project.
-            - Selecting Blue or Red on the View menu should change the imageList
-              attached to all buttons so that any current play will change the color
-              of all button images.
-            - Saved games should save to XML.  A game should load only from XML and
-              should not crash the application if a user tries to load an incorrect 
-              file.
-        */
+        private Pieces                 StartingPiece = Pieces.x;
+
+        private enum Colors            { blue, red }
+
+        private Colors                 CurrentColor  = Colors.blue;
+
+        private List< Button >         AllTiles;
+
+        private List< List< Button > > WinningRuns;
 
         public frmTicTacToe()
         {
             InitializeComponent();
+            CollectTiles();
+        }
+
+        private void loadGameToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            LoadGame();
+        }
+
+        private void saveGameToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            SaveGame();
+        }
+
+        private void exitToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            Close();
+        }
+
+        private void blueToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            SwitchColors( Colors.blue );
+        }
+
+        private void redToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            SwitchColors( Colors.red );
+        }
+
+        private void xToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            StartWith( Pieces.x );
+        }
+
+        private void oToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            StartWith( Pieces.o );
+        }
+
+        private void toolStripButton1_Click( object sender, EventArgs e )
+        {
+            // New Game
+        }
+
+        private void r1c1button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void r1c2button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void r1c3button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void r2c1button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void r2c2button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void r2c3button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void r3c1button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void r3c2button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void r3c3button_Click( object sender, EventArgs e )
+        {
+            button_Click( sender, e );
+        }
+
+        private void button_Click( object sender, EventArgs e )
+        {
+            var button = sender as Button;
+
+            button.Tag = StartingPiece;
+
+            CheckGameProgress();
+        }
+
+        private void CheckGameProgress()
+        {
+            if( WinningRuns.Any( run => 
+                run.All( tile => 
+                    StartingPiece == ( tile.Tag != null
+                        ? (Pieces) tile.Tag
+                        : Pieces.z ) ) ) )
+                MessageBox.Show( "Win" );
+        }
+
+        private void CollectTiles()
+        {
+            var nums    = Enumerable.Range( 1, 3 );
+            var runs    = "rc";
+
+            AllTiles    = Controls.OfType< Button >().ToList();
+
+            WinningRuns = 
+                new List< List< Button > > (
+                from run in runs
+                from num in nums
+                select new List< Button > (
+                    from tl in AllTiles
+                    where tl.Name.Contains( $"{ run }{ num }" )
+                    select tl ) );
+
+            WinningRuns.Add( new List< Button >(){ r1c1button, r2c2button, r3c3button } );
+            WinningRuns.Add( new List< Button >(){ r1c3button, r2c2button, r3c1button } );
+        }
+
+        private void LoadGame()
+        {
+        }
+
+        private void SaveGame()
+        {
+
+        }
+
+        private void SwitchColors( Colors color )
+        {
+
+        }
+
+        private void StartWith( Pieces piece )
+        {
+            switch( piece )
+            {
+                case Pieces.x:
+                case Pieces.o: break;
+                default:       throw( new ArgumentException( "Piece must be x or o!", "piece" ) );
+            }
         }
     }
 }
