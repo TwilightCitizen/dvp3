1d37837c1609574931d557e2f8c1036cd366725c
David Clark <daclark1@student.fullsail.edu>
2019-04-19 20:01:12 -0400

    Fluent State Machine Class Library and Test Driver Client App.  Fluent State Machine is a class library that supports the create of finite state machines with a fluent instantiation API.  The driver application is merely a console application to test the behavior of the class library with a Lamp FSM.  The Lamp FSM has two states--off and on--and one trigger--twist--that drives the state machine through state transitions and entry, exit, and transition actions.  Because the FSM runs in a separate thread, reading triggers off a queue, stopping it can halt the processing of triggers not yet dequeued.

---
+++ library/FluentStateMachine/FluentStateMachine/FluentStateMachine/FluentStateMachine/StateMachine.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace FluentStateMachine
+{
+	public class StateMachine< TState, TTrigger > where TState : Enum  where TTrigger : Enum 
+	{
+		public TState State   { get; private set; }
+		
+		public bool   Running { get; private set; }
+		
+		private ConcurrentQueue< TTrigger > Triggers = new ConcurrentQueue< TTrigger >();
+		
+		internal Dictionary< String, Action > TransitionActions = new Dictionary< String, Action >();
+		internal Dictionary< String, TState > Transitions       = new Dictionary< String, TState >();
+	
+		public StateMachine( TState startState )
+		{
+			State = startState;
+		}
+		
+		public ForState< TState, TTrigger > For( TState state )
+		{
+			return new ForState< TState, TTrigger >( this, state );
+		}
+		
+		public async void Start()
+		{
+			Running = true;
+			
+			await Task.Run( () =>
+			{
+				while( Running )
+				{
+					TTrigger trigger;
+					
+					if( Triggers.TryDequeue( out trigger ) )
+					{
+						var exitAction  = State + "-->";
+						var nextState   = Transitions[ State + "<->" + trigger ];
+						var transAction = State + "-->" + nextState;
+						var entryAction = "-->" + nextState;
+						
+						if( TransitionActions.ContainsKey( exitAction  ) ) TransitionActions[ exitAction  ].Invoke();
+						if( TransitionActions.ContainsKey( transAction ) ) TransitionActions[ transAction ].Invoke();
+						if( TransitionActions.ContainsKey( entryAction ) ) TransitionActions[ entryAction ].Invoke();
+						
+						State = nextState;
+					}
+					
+					Thread.Sleep( 0 );
+				}
+			} );
+		}
+		
+		public void Trigger( TTrigger trigger )
+		{
+			Triggers.Enqueue( trigger );
+		}
+		
+		public void Stop()
+		{
+			Running = false;
+		}
+	}
+	
+	public class ForState< TState, TTrigger > where TState : Enum  where TTrigger : Enum 
+	{
+		private StateMachine< TState, TTrigger > Machine;
+		private TState                           State;
+		
+		internal ForState( StateMachine< TState, TTrigger> machine, TState state )
+		{
+			Machine = machine;
+			State   = state;
+		}
+		
+		public StateMachine< TState, TTrigger > OnEntry( Action action )
+		{
+			Machine.TransitionActions[ "-->" + State ] = action;
+			
+			return Machine;
+		}
+		
+		public StateMachine< TState, TTrigger > OnExit( Action action )
+		{
+			Machine.TransitionActions[ State + "-->" ] = action;
+			
+			return Machine;
+		}
+		
+		public StateMachine< TState, TTrigger > On( TTrigger trigger, Action action, TState nextState )
+		{
+			Machine.Transitions[       State + "<->" + trigger   ] = nextState;
+			Machine.TransitionActions[ State + "-->" + nextState ] = action;
+			
+			return Machine;
+		}
+	}
+}
---
+++ library/FluentStateMachine/FluentStateMachine/TestDriverFSMClient/TestDriverFSMClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ClientApp
+{
+	using FluentStateMachine;
+	
+	public class Program
+	{
+		private enum State   { on, off }
+		private enum Trigger { twist   }
+		
+		private static StateMachine< State, Trigger > Lamp =
+			new StateMachine< State, Trigger >( State.off )
+				.For( State.off ).On( Trigger.twist, OffToOn, State.on  )
+				.For( State.on  ).On( Trigger.twist, OnToOff, State.off )
+				.For( State.on  ).OnEntry( EnteredOn  )
+				.For( State.on  ).OnExit(  LeftOn     )
+				.For( State.off ).OnEntry( EnteredOff )
+				.For( State.off ).OnExit(  LeftOff    );
+				
+		
+		public static void Main()
+		{
+			Console.Clear();
+			Lamp.Start();
+			Console.WriteLine( "Started" );
+			Lamp.Trigger( Trigger.twist );
+			Console.WriteLine( "Twisted" );
+			Lamp.Trigger( Trigger.twist );
+			Console.WriteLine( "Twisted" );
+			//Lamp.Stop();
+			//Console.WriteLine( "Stopped" );
+            Console.ReadLine();
+		}
+		
+		public static void OnToOff()
+		{
+			Console.WriteLine( "Lamp was turned off." );
+		}
+					 
+		public static void OffToOn()
+		{
+			Console.WriteLine( "Lamp was turned on." );
+		}
+		
+		public static void EnteredOn()
+		{
+			Console.WriteLine( "Lamp is on!" );
+		}
+		
+		public static void LeftOn()
+		{
+			Console.WriteLine( "Lamp is turning off!" );
+		}
+		
+		public static void EnteredOff()
+		{
+			Console.WriteLine( "Lamp is off!" );
+		}
+		
+		public static void LeftOff()
+		{
+			Console.WriteLine( "Lamp is turning on!" );
+		}
+	}
+}
